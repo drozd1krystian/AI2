30ad2e65a889de361584e5baba9bf1bf01d429f6
drozd1krystian <37024620+drozd1krystian@users.noreply.github.com>
2018-12-19 18:29:01 +0100

    Update tests.py

--- DjangoProject/projekt/tests.py
+++ DjangoProject/projekt/tests.py
@@ -5,32 +5,32 @@ import requests
 import numpy as np
 import quandl
 
+def test_object(nazwa="BTC", data = date.today(),cena = "3214"):
+    return Dane.objects.create(nazwa=nazwa, data = data,cena = cena)
+
 class DaneTestCase(TestCase):
 
-    def test_object(self,nazwa="BTC", data = date.today(),cena = "3214"):
-        return Dane.objects.create(nazwa=nazwa, data = data,cena = cena)
-
     def test_created_object(self):
-        sample_object = Dane.objects.create(nazwa="BTC", data = date.today(),cena = "3214")
+        sample_object = test_object()
         self.assertTrue(isinstance(sample_object, Dane))
 
-    def test_inserting(self):
-        sample_data = Dane.objects.create(nazwa="BTC", data = date.today(),cena = "3214")
+    def test_insert_and_filter(self):
+        sample_data = test_object()
         sample_data.save()
-
-    def test_filter(self):
         filter_data = Dane.objects.filter(nazwa = "BTC")
-        return len(filter_data) > 0
+        self.assertIsNot(len(filter_data), 0)
 
     def test_delete(self):
-        sample_object = Dane.objects.create(nazwa="BTC", data = date.today(),cena = "3214")
+        sample_object = test_object()
         sample_object.save()
         Dane.objects.all().delete()
+        filter_data = Dane.objects.filter(nazwa = "BTC")
+        self.assertIs(len(filter_data), 0)
 
     def test_dataFromQuandl(self):
         df = quandl.get("BITFINEX/BTCUSD",returns = "numpy", rows = "1")
-        return isinstance(df, np.ndarray)
+        self.assertIs(type(df),np.recarray)
 
     def test_actual_price(self):
         test_object = requests.get('https://api.bitfinex.com/v2/candles/trade:1m:tBTCUSD/hist').json()
-        return len(test_object) > 0
+        self.assertIsNot(len(test_object), 0)
