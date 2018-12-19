f2e71de0bea86b5e6d512ba60a434b0f27a3d1c9
drozd1krystian <37024620+drozd1krystian@users.noreply.github.com>
2018-12-19 15:16:49 +0100

    Update tests.py

--- DjangoProject/projekt/tests.py
+++ DjangoProject/projekt/tests.py
@@ -1,9 +1,36 @@
 from django.test import TestCase
 from .models import Dane
 from datetime import datetime, timedelta,date
-import response
+import requests
+import numpy as np
+import quandl
 
 class DaneTestCase(TestCase):
+
+    def test_object(self,nazwa="BTC", data = date.today(),cena = "3214"):
+        return Dane.objects.create(nazwa=nazwa, data = data,cena = cena)
+
+    def test_created_object(self):
+        sample_object = Dane.objects.create(nazwa="BTC", data = date.today(),cena = "3214")
+        self.assertTrue(isinstance(sample_object, Dane))
+
     def test_inserting(self):
-        Dane.objects.create(nazwa="BTC", data = date.today(),cena = "3214")
-        Dane.objects.create(nazwa="BTC", data = date.today()-timedelta(1),cena = "9999")
+        sample_data = Dane.objects.create(nazwa="BTC", data = date.today(),cena = "3214")
+        sample_data.save()
+
+    def test_filter(self):
+        filter_data = Dane.objects.filter(nazwa = "BTC")
+        return len(filter_data) > 0
+
+    def test_delete(self):
+        sample_object = Dane.objects.create(nazwa="BTC", data = date.today(),cena = "3214")
+        sample_object.save()
+        Dane.objects.all().delete()
+
+    def test_dataFromQuandl(self):
+        df = quandl.get("BITFINEX/BTCUSD", rows = "1")
+        isinstance(df, np.ndarray)
+
+    def test_actual_price(self):
+        test_object = requests.get('https://api.bitfinex.com/v2/candles/trade:1m:tBTCUSD/hist').json()
+        return len(test_object) > 0
